66594811fefc7ab50cf4c90952827104919869e0
Yurii <puklo24@gmail.com>
2017-08-19 13:54:39 +0300

    1

--- img/dynamic_img.js
+++ img/dynamic_img.js
@@ -1,11 +1,15 @@
 var imgFiles = ['1.jpg', '2.jpg', '3.png'];
-var dynImg = jQuery.cookie('dynImg');
+var minutes = 2;
 
+var expiryDate = new Date();
+expiryDate.setTime(date.getTime() + (minutes * 60 * 1000));
+
+var dynImg = jQuery.cookie('dynImg');
 if (dynImg !== undefined) {
     displayImage(dynImg);
 } else {
     var item = imgFiles[Math.floor(Math.random()*imgFiles.length)];    
-    jQuery.cookie('dynImg', item);
+    jQuery.cookie('dynImg', item, { expires: expiryDate });
     displayImage(item);
 }
 
