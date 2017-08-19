226be9f5325e09441e972ea2e692c9b997c635de
Yurii <puklo24@gmail.com>
2017-08-19 13:34:45 +0300

    1

--- img/dynamic_img.js
+++ img/dynamic_img.js
@@ -1,12 +1,13 @@
 var imgNames = ["1.jpg", "2.jpg"];
 
-var imgName = Cookies.get('dynImg');
+var imgName = jQuery.cookie(dynImg);
 
 if (imgName !== undefined) {
     displayImage(img);
 } else {
     var item = imgNames[Math.floor(Math.random()*imgNames.length)];    
-    Cookies.set('dynImg', item, { expires: 0 });
+    // Cookies.set('dynImg', item, { expires: 0 });
+    jQuery.cookie('dynImg', item);
     displayImage(item);
 }
 
