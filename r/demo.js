80f0b531d0b3b2fc0bcf7c7f0ab12cc1c8880904
Yurii <puklo24@gmail.com>
2017-08-26 07:46:11 +0300

    212

---
+++ r/demo.js
@@ -0,0 +1,17 @@
+// Set up the canvas dimensions
+var canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d');
+canvas.width = 650;
+canvas.height = 450;
+
+// Grab the iframe
+var inner = document.getElementById('map');
+
+// Get the image
+iframe2image(inner, function (err, img) {
+  // If there is an error, log it
+  if (err) { return console.error(err); }
+
+  // Otherwise, add the image to the canvas
+  context.drawImage(img, 0, 0);
+});
